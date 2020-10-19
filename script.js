0bed5e862af413ae9d8f90b2681a8101705a3f26
paigekobz <44929206+paigekobz@users.noreply.github.com>
2020-10-18 20:19:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log('hi');
+
+
+ 
+function button ()
+
+{window.prompt("What are some reasons you waste food?")
+ 
+ window.prompt( "Do you use a compost bin at home/work/school?")
+ 
+ window.prompt("Have you ever donated excess non-perishables to a food bank?")
+ 
+ window.prompt("How much food do you typically throw out each week?")
+ 
+ window.prompt("What are some reasons you buy more food than you need?")
+ 
+ window.alert ("Thank you for taking our survey!")
+                  }
